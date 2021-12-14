40e84c0694a5c0735ac93bca885932504816943a
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2021-12-14 10:09:15 +0000

    Added a test to check the bike is released, made test pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require "docking_station"
 describe DockingStation do 
   it {should respond_to(:release_bike)}
+  it "should release a bike" do
+    expect(subject.release_bike).to be_a(Bike)
+  end
 end 
 
 
