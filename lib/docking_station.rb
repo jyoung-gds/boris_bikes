ce132b1b10ddf3d57c7946a78db9a2138c347961
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2021-12-16 14:52:29 +0000

    Completed chapter 12, all green rspecs

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,13 @@
 class DockingStation
 
   attr_reader :docked_bikes
-
   def release_bike
+    fail "No bikes available" unless @docked_bikes
     Bike.new
   end
 
   def dock_bike(bike)
+    fail "Docking station is full" unless @docked_bikes == nil
     @docked_bikes = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,31 +4,35 @@ describe DockingStation do
   it { should respond_to(:release_bike) }
 
   it "should release a working bike" do
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
+    subject.dock_bike(Bike.new)
+    new_bike = subject.release_bike
 
-    expect(bike.working?).to be true
+    expect(new_bike.working?).to be true
   end
 
   it { should respond_to(:dock_bike) }
 
-  it "docks a bike in the docking station" do
-    station = DockingStation.new
-    bike = station.release_bike
+  it "stores a bike in the docking station when the user docks it" do
+    subject.dock_bike(Bike.new)
 
-    station.dock_bike(bike)
-
-    expect(station.docked_bikes).to eq bike
+    expect(subject.docked_bikes).to eq bike
   end
 
   it { should respond_to(:view_bike) }
 
-  it "shows which bikes are in the dock" do
-    station = DockingStation.new
-    station.dock_bike(Bike.new)
+  it "allows the user to view which bikes are in the dock" do
+    subject.dock_bike(Bike.new)
 
-    expect(station.view_bike).to eq(station.docked_bikes)
+    expect(subject.view_bike).to eq(subject.docked_bikes)
   end
 
-end 
+  it "raises an error when trying to release bikes from an empty docking station" do
+    expect{subject.release_bike}.to raise_error(RuntimeError, "No bikes available")
+  end
 
+  it "raises an error when trying to dock bikes to a full docking station" do
+    subject.dock_bike(Bike.new)
+    expect{subject.dock_bike(Bike.new)}.to raise_error(RuntimeError, "Docking station is full")
+  end
+
+end
